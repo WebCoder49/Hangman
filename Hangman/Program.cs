0df017dc9a3a007aba3f66d0ad333f30dc0ffd0b
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-10-02 21:47:55 +0100

    Write main working game

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -17,7 +17,90 @@ namespace Hangman
             // Console.WriteLine(GameStatus.OutOfGuesses(7));
             // Console.WriteLine(GameStatus.WordGuessed("hello", "hello"));
 
+            // TEST Gallow
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     Console.WriteLine($"Guess #{i}");
+            //     Console.Write(Gallow.DrawGallow(i));
+            // }
+            
+            WordPicker wordPicker = new();
+            Game(wordPicker.PickWord(WordPicker.Difficulty.Easy));
+        }
 
+        static void Game(string wordStr)
+        {
+            List<char> word = new(wordStr);
+            char[] guessedWord = new char [word.Count];
+            // Copy length of word to guessedWord as underscore characters
+            for (int i = 0; i < word.Count; i++)
+            {
+                guessedWord[i] = '_';
+            }
+            
+            int guesses = 0;
+            List<char> wrongLetters = new();
+            while (true) // Return to break out of loop
+            {
+                Console.Clear();
+                
+                // TODO (ahe127) Display letters and dashes
+                Console.Write($"Wrong letters:   ");
+                Console.Write(String.Join(", ", wrongLetters));
+                Console.WriteLine();
+                Console.Write($"Correct letters: ");
+                Console.Write(guessedWord);
+                Console.WriteLine();
+                
+                Console.Write(Gallow.DrawGallow(guesses));
+                
+                if (GameStatus.WordGuessed(new String(guessedWord), wordStr))
+                {
+                    Console.WriteLine($"You Guessed Correctly!");
+                    return; // Won
+                }
+                
+                Console.Write("Enter your guess for a letter: ");
+                char guessedLetter = Console.ReadLine().ToUpper()[0];
+                // TODO (SophiaNass) Validate
+                
+                if (GameStatus.IsGuessValid(guessedLetter, wrongLetters))
+                {
+                        
+                    List<int> letterIndexes = GetLetterIndexes(guessedLetter, word);
+                    for (int i = 0; i < letterIndexes.Count; i++)
+                    {
+                        guessedWord[letterIndexes[i]] = guessedLetter;
+                    }
+
+                    if (letterIndexes.Count == 0)
+                    {
+                        // Letter not in word
+                        wrongLetters = wrongLetters.Append(guessedLetter).ToList();
+                    
+                        guesses++;
+                        if (guesses >= 7)
+                        {
+                            Gallow.DrawGallow(guesses);
+                            return; // Lost
+                        }
+                    }
+                }
+            }
+        }
+
+        public static List<int> GetLetterIndexes(char chr, List<char> word)
+        {
+            // TODO (ahe127) replace with your function
+            List<int> letterIndexes = new();
+            for (int i = 0; i < word.Count; i++)
+            {
+                if (word[i] == chr)
+                {
+                    letterIndexes = letterIndexes.Append(i).ToList();
+                }
+            }
+            return letterIndexes;
         }
     }
 }
