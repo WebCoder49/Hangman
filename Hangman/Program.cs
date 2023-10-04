b5103552fe35a49f8b4237ba2248a0c99f7e7f0e
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-10-04 14:10:33 +0100

    Fix merge conflicts

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -28,85 +28,6 @@ namespace Hangman
             // }
             
             WordPicker wordPicker = new();
-<<<<<<< HEAD
-            Game(wordPicker.PickWord(WordPicker.Difficulty.Easy));
-
-            static void Game(string wordStr)
-            {
-                List<char> word = new(wordStr);
-                char[] guessedWord = new char [word.Count];
-                // Copy length of word to guessedWord as underscore characters
-                for (int i = 0; i < word.Count; i++)
-                {
-                    guessedWord[i] = '_';
-                }
-
-                int guesses = 0;
-                List<char> wrongLetters = new();
-                while (true) // Return to break out of loop
-                {
-                    Console.Clear();
-
-                    // TODO (ahe127) Display letters and dashes
-                    Console.Write($"Wrong letters:   ");
-                    Console.Write(String.Join(", ", wrongLetters));
-                    Console.WriteLine();
-                    Console.Write($"Correct letters: ");
-                    Console.Write(guessedWord);
-                    Console.WriteLine();
-
-                    Console.Write(Gallow.DrawGallow(guesses));
-
-                    if (GameStatus.WordGuessed(new String(guessedWord), wordStr))
-                    {
-                        Console.WriteLine($"You Guessed Correctly!");
-                        return; // Won
-                    }
-
-                    Console.Write("Enter your guess for a letter: ");
-                    char guessedLetter = Console.ReadLine().ToUpper()[0];
-                    // TODO (SophiaNass) Validate
-
-                    if (GameStatus.IsGuessValid(guessedLetter, wrongLetters))
-                    {
-
-                        List<int> letterIndexes = GetLetterIndexes(guessedLetter, word);
-                        for (int i = 0; i < letterIndexes.Count; i++)
-                        {
-                            guessedWord[letterIndexes[i]] = guessedLetter;
-                        }
-
-                        if (letterIndexes.Count == 0)
-                        {
-                            // Letter not in word
-                            wrongLetters = wrongLetters.Append(guessedLetter).ToList();
-
-                            guesses++;
-                            if (guesses >= 7)
-                            {
-                                Gallow.DrawGallow(guesses);
-                                return; // Lost
-                            }
-                        }
-                    }
-                }
-            }
-
-            static List<int> GetLetterIndexes(char chr, List<char> word)
-            {
-                // TODO (ahe127) replace with your function
-                List<int> letterIndexes = new();
-                for (int i = 0; i < word.Count; i++)
-                {
-                    if (word[i] == chr)
-                    {
-                        letterIndexes = letterIndexes.Append(i).ToList();
-                    }
-                }
-
-                return letterIndexes;
-            }
-=======
             // Game loop
             while (true)
             {
@@ -143,7 +64,6 @@ namespace Hangman
                     Game((string)word);
                 }
             }
->>>>>>> 807a49e0d00dfe4737be1e84b41fa7cd766b791c
         }
 
         public static void Game(string wordStr)
